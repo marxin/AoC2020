665e7c4e4c7f4f4d77d3d6ff8fdd0b68765a8bae
marxin <mliska@suse.cz>
2020-12-14 18:16:08 +0100

    Finish day 14.

--- day14/solve.py
+++ day14/solve.py
@@ -9,17 +9,46 @@ mask_and = None
 mask_or = None
 mem = {}
 
+def generate_addresses(indexstr, addresses):
+    if 'X' in indexstr:
+        i = indexstr.index('X')
+        indexstr[i] = '0'
+        generate_addresses(indexstr, addresses)
+        indexstr[i] = '1'
+        generate_addresses(indexstr, addresses)
+        indexstr[i] = 'X'
+    else:
+        addresses.append(''.join(indexstr))
+
+def combine(mask, index):
+    index = bin(index)[2:].rjust(BITS, '0')
+    assert len(index) == len(mask)
+    index = list(index)
+    for i, v in enumerate(mask):
+        if v == '0':
+            pass
+        elif v == '1':
+            index[i] = '1'
+        elif v == 'X':
+            index[i] = 'X'
+        else:
+            assert False
+    addresses = []
+    generate_addresses(index, addresses)
+    return addresses
+
+mask = None
+
 for line in lines:
     lhs, rhs = line.split(' = ')
     if lhs == 'mask':
         assert len(rhs) == BITS
-        mask_and = int(rhs.replace('X', '1'), 2)
-        mask_or = int(rhs.replace('X', '0'), 2)
+        mask = rhs
     else:
         assert lhs.startswith('mem')
         index = int(lhs.replace('[', ']').split(']')[1])
         value = int(rhs)
-        mem[index] = (value & mask_and) | mask_or
-        print(f'{mem[index]:b}')
+        for address in combine(mask, index):
+            mem[int(address, 2)] = value
 
 print(sum(mem.values()))
