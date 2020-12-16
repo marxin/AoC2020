d645358ce90e864fe76648732fa259cc342e091d
marxin <mliska@suse.cz>
2020-12-16 12:40:41 +0100

    Finish day 16.

--- day16/solve.py
+++ day16/solve.py
@@ -29,21 +29,60 @@ wagon: 39-79 or 86-969
 zone: 48-306 or 317-974
 '''
 
+myticket = [191,89,73,139,71,103,109,53,97,179,59,67,79,101,113,157,61,107,181,137]
+
+d = {}
 rules = set()
 for line in rules_input.strip().splitlines():
-    ranges = line.split(':')[1].strip()
+    parts = line.split(': ')
+    name = parts[0]
+    d[name] = set()
+    ranges = parts[1].strip()
     for rule in ranges.split(' or '):
         start, end = [int(x) for x in rule.split('-')]
         for i in range(start, end + 1):
             rules.add(i)
+            d[name].add(i)
 
-print(rules)
+valid = []
 
 suma = 0
 for line in open('input.txt').read().splitlines():
     numbers = [int(x) for x in line.split(',')]
-    for n in numbers:
-        if n not in rules:
-            suma += n
+    invalid = [n for n in numbers if n not in rules]
+    suma += sum(invalid)
+    if not invalid:
+        valid.append(numbers)
+        # print(numbers)
 
 print(suma)
+print(f'valid: {len(valid)}')
+
+valid_indices = {}
+
+for key in d:
+    valid_indices[key] = set()
+    for i in range(len(d)):
+        if all([v[i] in d[key] for v in valid]):
+            valid_indices[key].add(i)
+
+mapping = {}
+
+while len(mapping) != len(d):
+    for key in valid_indices:
+        values = list(valid_indices[key])
+        if len(values) == 1:
+            index = values[0]
+            mapping[key] = index
+            for key, value in valid_indices.items():
+                value.discard(index)
+            break
+
+print(mapping)
+mult = 1
+
+for k, v in mapping.items():
+    if 'departure' in k:
+        mult *= myticket[v]
+
+print(mult)
