8bb9d2804e25295870823f175a757877da4fbbc6
marxin <mliska@suse.cz>
2020-12-20 10:34:22 +0100

    Finish day 19.

--- day19/solve.py
+++ day19/solve.py
@@ -13,29 +13,45 @@ for line in first.splitlines():
     rules[lhs] = parts
 
 data = second.splitlines()
-print(rules)
 
-start = rules['0']
-while any([c for c in start if c.isdigit()]):
-    print(start)
-    start2 = []
-    for i, v in enumerate(start):
-        if v.isdigit():
-            start2.append('(')
-            start2 += rules[v]
-            start2.append(')')
-        else:
-            start2 += v
-    start = start2
+def create_regex(x, y):
+    start = rules['0']
+    while any([c for c in start if c.isdigit()]):
+        start2 = []
+        for i, v in enumerate(start):
+            if v.isdigit():
+                start2.append('(')
+                if v == '8' and x > 0:
+                    x -= 1
+                    start2 += ['42', '8']
+                elif v == '11' and y > 0:
+                    y -= 1
+                    start2 += ['42', '11', '31']
+                else:
+                    start2 += rules[v]
+                start2.append(')')
+            else:
+                start2 += v
+        start = start2
+    regex = re.compile(''.join(start))
+    return regex
 
-print(start)
-regex = re.compile(''.join(start))
-print(regex)
+matched = set()
 
-c = 0
-for line in data:
-    m = regex.fullmatch(line)
-    if m:
-        c +=1
+print(len(data))
 
-print(c)
+def measure(regex):
+    for line in data:
+        if line in matched:
+            continue
+        m = regex.fullmatch(line)
+        if m:
+            matched.add(line)
+
+for i in range(0, 10):
+    for j in range(0, 10):
+        print(i, j)
+        regex = create_regex(i, j)
+        measure(regex)
+
+print(len(matched))
